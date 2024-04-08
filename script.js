9f83aa379e4bd427a4f50085628dd25baf2b63af
Eduardo Damasceno <102323022+EduardoDama@users.noreply.github.com>
2024-04-08 13:19:12 -0300

    primeira versão

---
+++ script.js
@@ -0,0 +1,19 @@
+var imagens = document.querySelectorAll('.minhaImagem');
+  imagens.forEach(function(imagem) {
+    imagem.addEventListener('click', function() {
+      abrirPopup(imagem);
+    });
+  });
+
+  // Função para abrir o popup com a imagem clicada
+  function abrirPopup(imagem) {
+    var popup = document.getElementById("meuPopup");
+    var imagemPopup = document.getElementById("imagemPopup");
+    imagemPopup.src = imagem.src;
+    popup.style.display = "block";
+  }
+
+  // Função para fechar o popup
+  function fecharPopup() {
+    document.getElementById("meuPopup").style.display = "none";
+  }
